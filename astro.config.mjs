e258c35df512039dae925fd72920ef0143d65096
amirmtaati <amirmtaati@gmail.com>
2025-05-04 11:59:47 +0330

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import UnoCSS from 'unocss/astro';
 export default defineConfig({
   integrations: [UnoCSS()],
   site: 'https://amirmtaati.github.io', // Replace with your GitHub Pages URL
-  base: '/amirmtaati.github.io', // Replace with your repository name, e.g., '/my-blog'
+  base: '/', // Replace with your repository name, e.g., '/my-blog'
   output: 'static', // Ensure static site generation
 });
