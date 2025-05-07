d85b72875e883fbda71b9ecd1b185674d77d520f
amirmtaati <amirmtaati@gmail.com>
2025-05-07 21:18:09 +0330

    some new changes made

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
+import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math'; // Add this import
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  integrations: [UnoCSS()],
+  integrations: [UnoCSS(), mdx()],
   site: 'https://amirmtaati.github.io', // Replace with your GitHub Pages URL
   base: '/', // Replace with your repository name, e.g., '/my-blog'
   output: 'static', // Ensure static site generation,
