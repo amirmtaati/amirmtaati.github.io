0adda0a5a0773c21067f58d6f7ffb341a8914bbf
amirmtaati <amirmtaati@gmail.com>
2025-07-26 17:32:31 +0330

    new blog style

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Amir's Technical Notes";
+export const SITE_DESCRIPTION = 'Thoughtful essays, tutorials, and perspectives on programming, systems, and computing culture.';
