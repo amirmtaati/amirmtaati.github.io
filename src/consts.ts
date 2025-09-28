aa9784c6201c6c4b48684dec703606e7059ef7cc
amirmtaati <amirmtaati@gmail.com>
2025-09-28 19:55:54 +0330

    removed haha

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Amir's Technical Notes";
-export const SITE_DESCRIPTION = 'Thoughtful essays, tutorials, and perspectives on programming, systems, and computing culture.';
