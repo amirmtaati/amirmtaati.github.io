54ec976298063a8bd7ea881206e18b44c7695a6f
amirmtaati <amirmtaati@gmail.com>
2025-09-29 21:21:29 +0330

    code blocks

--- build.el
+++ build.el
@@ -1,4 +1,3 @@
-;; Load required packages for syntax highlighting
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
@@ -8,11 +7,18 @@
   (package-install 'htmlize))
 
 (require 'ox-publish)
+(require 'htmlize)
 
-;; Set a clean theme for htmlize
-(setq htmlize-html-major-mode 'html-mode)
-(load-theme 'modus-vivendi t t)  ; Light theme for clean code block output
-(setq htmlize-use-rgb-map 'modus-vivendi)  ; Ensure consistent colors
+(custom-set-faces
+ '(font-lock-keyword-face ((t (:foreground "#569cd6"))))
+ '(font-lock-string-face ((t (:foreground "#ce9178"))))
+ '(font-lock-comment-face ((t (:foreground "#6a9955"))))
+ '(font-lock-function-name-face ((t (:foreground "#dcdcaa"))))
+ '(font-lock-variable-name-face ((t (:foreground "#9cdcfe"))))
+ '(font-lock-type-face ((t (:foreground "#4ec9b0"))))
+ '(default ((t (:background "#2b2b2b" :foreground "#d4d4d4")))))
+
+(setq org-html-htmlize-output-type 'css)
 
 (setq org-publish-project-alist
       (list
